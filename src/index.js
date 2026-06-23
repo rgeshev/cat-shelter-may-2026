6014b20cdd31f1ac3d77afe9048d49d69c6a7717
rgeshev <Rosen_Geshev19@abv.bg>
2026-06-23 10:46:57 +0300

    Added http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello from the Cat Shelter Workshop!');
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.write('Hello, World!');
+    res.end();
+});
+
+server.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
+
+
