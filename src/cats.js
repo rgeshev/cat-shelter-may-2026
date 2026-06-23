41ae2c66729a0e8bc27c0dd300dc67f92135b711
rgeshev <Rosen_Geshev19@abv.bg>
2026-06-23 13:28:57 +0300

    Showing cats dynamically based on an array

---
+++ src/cats.js
@@ -0,0 +1,63 @@
+const cats = [
+  {
+    id: 1,
+    name: "",
+    image:
+      "https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg",
+    breed: "Bombay Cat",
+    description:
+      "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+    price: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 2,
+    name: "Pretty Kitty",
+    image: "https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg",
+    breed: "Bombay Cat",
+    description:
+      "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+    price: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 3,
+    name: "Pretty Kitty",
+    image:
+      "https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg",
+    breed: "Bombay Cat",
+    description:
+      "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+    price: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 4,
+    name: "Pretty Kitty",
+    image:
+      "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg",
+    breed: "Bombay Cat",
+    description:
+      "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+    price: 350,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+  {
+    id: 5,
+    name: "Pretty Kitty",
+    image: "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+    breed: "Bombay Cat",
+    description:
+      "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+    price: null,
+    createdAt: new Date(),
+    updatedAt: null,
+  },
+];
+
+
+export default cats;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,70 @@
-import http from 'http';
-import fs from 'fs/promises';
+import http from "http";
+import fs from "fs/promises";
+import cats from "./cats.js";
 
 const server = http.createServer(async (req, res) => {
-    if (req.url === '/styles/site.css') {
-        const cssContent = await fs.readFile('./src/styles/site.css', 'utf-8');
+  if (req.url === "/styles/site.css") {
+    const cssContent = await fs.readFile("./src/styles/site.css", "utf-8");
 
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.write(cssContent);
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.write(cssContent);
 
-        return res.end();
-    }
+    return res.end();
+  }
 
-    if (req.url === '/js/script.js') {
-        const jsContent = await fs.readFile('./src/js/script.js', 'utf-8');
+  if (req.url === "/js/script.js") {
+    const jsContent = await fs.readFile("./src/js/script.js", "utf-8");
 
-        res.writeHead(200, { 'Content-Type': 'text/javascript'});
-        res.write(jsContent);
-        return res.end();
-    }
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+    res.write(jsContent);
+    return res.end();
+  }
 
-    let htmlContent = '';
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    
-    switch (req.url) {
-        case '/':
-            htmlContent = await fs.readFile('./src/views/home/index.html', 'utf-8');
-            break;
-        case '/cats/add-breed':
-            htmlContent = await fs.readFile('./src/views/addBreed.html', 'utf-8');
-            break;
-        case '/cats/add-cat':
-            htmlContent = await fs.readFile('./src/views/addCat.html', 'utf-8');
-            break;
-        default:
-            htmlContent = await fs.readFile('./src/views/notFound.html', 'utf-8');
-            break;   
-    };
+  let htmlContent = "";
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-    res.write(htmlContent);
-    res.end();
+  switch (req.url) {
+    case "/":
+      htmlContent = await renderHomePage();
+      break;
+    case "/cats/add-breed":
+      htmlContent = await fs.readFile("./src/views/addBreed.html", "utf-8");
+      break;
+    case "/cats/add-cat":
+      htmlContent = await fs.readFile("./src/views/addCat.html", "utf-8");
+      break;
+    default:
+      htmlContent = await fs.readFile("./src/views/notFound.html", "utf-8");
+      break;
+  }
+
+  res.write(htmlContent);
+  res.end();
 });
 
-server.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
+server.listen(5000, () =>
+  console.log("Server is listening on http://localhost:5000..."),
+);
 
+async function renderHomePage() {
+  let htmlContent = await fs.readFile("./src/views/home/index.html", "utf-8");
 
+  const catTemplate = (cat) => `
+    <li>
+        <img src="${cat.image}" alt="${cat.name}">
+        <h3>${cat.name}</h3>
+        <p><span>Breed: </span>${cat.breed}</p>
+        <p><span>Description: </span>${cat.description}</p>
+            <ul class="buttons">
+                <li class="btn edit"><a href="">Change Info</a></li>
+                <li class="btn delete"><a href="">New Home</a></li>
+            </ul>
+    </li>
+    `;
+
+    const catsContent = `<ul>${cats.map(cat => catTemplate(cat)).join('\n')}</ul>`
+
+    const result =htmlContent.replace('{{cats}}', catsContent);
+
+  return result;
+}
